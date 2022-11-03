40be9c3f412a041e3e4592e7da709efb5b97a167
siouyuchang <113596344+siouyuchang@users.noreply.github.com>
2022-11-03 20:01:12 +0800

    Add files via upload

---
+++ solver.f90
@@ -0,0 +1,32 @@
+module Solver 
+
+    implicit none
+    contains
+
+    subroutine rk2(n, yin, ynext, t, h, func)
+        implicit none
+        integer, intent(in) :: n      ! number of ODEs
+        real, intent(in)    :: t, h
+        real, dimension(n), intent(in)  :: yin
+        real, dimension(n), intent(out)  :: ynext
+        external      :: func
+        integer            :: i
+        real, dimension(n) :: k1, k2
+        real,dimension(n)  :: y2
+	  ! compute k1 = func(t, yin)
+        call func(n, t, yin, k1)
+        ! compute y2 = yin + h*k1
+  	  do i=1,n
+		y2(i) = yin(i) + h*k1(i)
+	  enddo
+        ! compute k2 = func(t+h, y2)
+	  call func(n, t+h,y2, k2)
+        ! compute ynext
+	  do i=1,n 
+		ynext(i)=yin(i) + (h/2.0) * (k1(i)+k2(i))
+	  enddo
+
+        return
+    end subroutine rk2
+
+end module Solver
